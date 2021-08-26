1ede41b9eb0d47fd8400ea020e31a67d0c5354b8
Nicholas Ryan Taylor <ntaylor6422@gmail.com>
2021-08-26 21:57:44 +0900

    added remove todo functionality

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-export default function Todo({ text }) {
+export default function Todo({ text, listKey, setAllTodos }) {
+  const handleDelete = () => {
+    setAllTodos((prevTodos) => {
+      const newOutput = [];
+      prevTodos.forEach((todo, i) => {
+        if (i !== listKey) newOutput.push(todo);
+      });
+      return newOutput;
+    });
+  };
   return (
     <>
       <h1>{text}</h1>
+      <button type="button" onClick={handleDelete}>
+        X
+      </button>
     </>
   );
 }
