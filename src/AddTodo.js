628cda7615dcf82defb88ee5493612a11194f8d7
Nicholas Ryan Taylor <ntaylor6422@gmail.com>
2021-08-26 21:32:03 +0900

    cleaned up some syntax

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,17 +1,18 @@
-import React from 'react';
-                                    //{} -> deconstructor
-export default function AddTodo ({currentTodo, setCurrentTodo, setAllTodos}) {
-    const handleChange = (e) => {
-        setCurrentTodo(e.target.value)
-    }
-    const handleSubmit = () => {
-        console.log(currentTodo)
-        setAllTodos((prevTodo) => [...prevTodo, currentTodo])
-    }
-    return (
-        <>
-            <input value = {currentTodo} onChange = {handleChange}/>
-            <button type = 'button' onClick = {handleSubmit}>Add</button>
-        </>
-    )
+import React from "react";
+//{} -> deconstructor
+export default function AddTodo({ currentTodo, setCurrentTodo, setAllTodos }) {
+  const handleChange = (e) => {
+    setCurrentTodo(e.target.value);
+  };
+  const handleSubmit = () => {
+    setAllTodos((prevTodo) => [...prevTodo, currentTodo]);
+  };
+  return (
+    <>
+      <input value={currentTodo} onChange={handleChange} />
+      <button type="button" onClick={handleSubmit}>
+        Add
+      </button>
+    </>
+  );
 }
--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, {useState} from 'react'; 
-import AddTodo from './AddTodo';
-import Todo from './Todo';
+import React, { useState } from "react";
+import AddTodo from "./AddTodo";
+import Todo from "./Todo";
 
-
-export default function App () {
-    const [currentTodo, setCurrentTodo] = useState("Hello")
-    const [allTodos, setAllTodos] = useState([])
-    return (
-                //key for uniquely identifying array items
-        <>
-            <AddTodo currentTodo = {currentTodo} setCurrentTodo = {setCurrentTodo} setAllTodos = {setAllTodos}/>
-            {allTodos.map( (todo, i) => <Todo text = {todo} key = {i}/>)}
-        </>
-    )
+export default function App() {
+  const [currentTodo, setCurrentTodo] = useState("");
+  const [allTodos, setAllTodos] = useState([]);
+  return (
+    //key for uniquely identifying array items
+    <>
+      <AddTodo
+        currentTodo={currentTodo}
+        setCurrentTodo={setCurrentTodo}
+        setAllTodos={setAllTodos}
+      />
+      {allTodos.map((todo, i) => (
+        <Todo text={todo} key={i} />
+      ))}
+    </>
+  );
 }
