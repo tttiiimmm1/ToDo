e27dfec2a9d8b5f74316b30b506cf0a4c76b7116
Tim Engeman <tim.engeman@gmail.com>
2021-08-26 22:27:18 +0900

    Clear text box on add

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -6,6 +6,7 @@ export default function AddTodo({ currentTodo, setCurrentTodo, setAllTodos }) {
   };
   const handleSubmit = () => {
     setAllTodos((prevTodo) => [...prevTodo, currentTodo]);
+    setCurrentTodo("");
   };
   return (
     <>
